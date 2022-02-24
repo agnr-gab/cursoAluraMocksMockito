f9cc000ba164f679af37034337f979d88617f6c0
Arê Nascimento <aretuza.gab@gmail.com>
2022-02-23 21:49:19 -0300

    Inserir enviador de emails

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -22,11 +22,14 @@ class FinalizarLeilaoServiceTest {
 	
 	@Mock
 	private LeilaoDao leilaoDao;
+
+	@Mock
+	private EnviadorDeEmails enviadorDeEmails;
 	
 	@BeforeEach
 	public void beforeEach() {
 		MockitoAnnotations.initMocks(this);
-		this.service = new FinalizarLeilaoService(leilaoDao);
+		this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
 	}
 
 	@Test
