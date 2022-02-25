916f265a9d281e938a38b3542b05754990824c36
Arê Nascimento <aretuza.gab@gmail.com>
2022-02-24 23:10:01 -0300

    Criar teste usando @Captor

--- src/test/java/GeradorDePagamentoTest.java
+++ src/test/java/GeradorDePagamentoTest.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class GeradorDePagamentoTest {
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.service.GeradorDePagamento;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeradorDePagamentoTest {
+    @Autowired
+    private GeradorDePagamento geradorDePagamento;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.geradorDePagamento = new GeradorDePagamento(pagamentoDao);
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao() {
+        Leilao leilao = leilao();
+        Lance lanceVencedor = leilao.getLanceVencedor();
+        geradorDePagamento.gerarPagamento(lanceVencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+
+        Pagamento pagamento = captor.getValue();
+        Assertions.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+
+        /*
+
+Ele é criado passando esse leilão, o usuário, o valor do lance, e a data de vencimento. Preciso verificar se todas essas informações foram preenchidas corretamente, então ‘Assert assertEquals’, a primeira coisa que quero verificar é a data de vencimento do meu pagamento, tem que ser amanhã, é um dia seguinte.
+
+Então aqui posso passar ‘LocalDate.now().plusDays(1)’, e vou verificar se é igual a ‘pagamento.getVencimento’. A primeira coisa que vou verificar.
+         */
+    }
+
+    private Leilao leilao() {
+
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance lance = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(lance);
+        leilao.setLanceVencedor(lance);
+
+        return leilao;
+    }
+
 }
